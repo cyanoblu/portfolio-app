5312373fcc965faf82e5c87d22d2d5c5d9a047be
cyanoblu <barbosa.raphael@pm.me>
2024-07-12 01:13:09 -0300

    every breath you take

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import mixpanel from 'mixpanel-browser';
+ 
+mixpanel.init(`${import.meta.env.VITE_MIXPANEL_TOKEN}`, {debug: true, track_pageview: true, persistence: 'localStorage', ignore_dnt: false});
 
 const app = createApp(App)
 
--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '/mixpanel': {
+        target: 'https://api-js.mixpanel.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/mixpanel/, ''),
+      },
+    }
   }
 })
